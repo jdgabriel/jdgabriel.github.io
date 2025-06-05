f4ce58437ddb04936e33ad89485666f65cb043cb
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2025-06-05 15:43:50 -0300

    feat: enhance build configuration with manual chunking for better optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,32 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    minify: 'esbuild', 
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor'
+            }
+            if (id.includes('@react-pdf/renderer')) {
+              return 'pdf-vendor'
+            }
+            if (id.includes('react-markdown')) {
+              return 'markdown-vendor'
+            }
+            if (id.includes('react-pdf-tailwind')) {
+              return 'pdf-tailwind-vendor'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
   plugins: [
     react(), 
     tailwindcss()
